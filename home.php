05d197f223da68099d4ec0c5a6ee8c5991973153
shiraga0830 <167880787+shiraga0830@users.noreply.github.com>
2025-10-30 14:56:01 +0900

    Update home.php

--- home.php
+++ home.php
@@ -2,32 +2,36 @@
 <div id="page-header">
 	<div id="page-inner">
 		<div id="page-text">
-			<h2 class="title topfade"><?php _e('News', 'sample'); ?></h2>
+			<h2 class="title topfade"><?php _e('News', 'tam'); ?></h2>
 		</div>
 	</div>
 </div>
 <?php output_breadcrumb(); ?>
-<div id="wrapper">
-	<div id="content">
-<?php $args = array(
+<div id="content">
+	<div class="inner">
+<?php
+$args = array(
 	'post_type' => 'post',
 	'posts_per_page' => 8,
 	'paged' => $paged
-); ?>
-<?php $the_query = new WP_Query($args); ?>
-<?php if(have_posts()) : ?>
-<?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+);
+$the_query = new WP_Query($args);
+if(have_posts()) :
+	while ($the_query->have_posts()) : $the_query->the_post();
+?>
 			<dl class="news">
-				<dt><?php the_time('Y/m/d');?><?php
+				<dt><?php the_time('Y/m/d'); ?><?php
 $category = get_the_category();
 if ($category[0]) {
 	echo '<a class="cate" href="' . get_category_link($category[0]->term_id) . '">' . $category[0]->cat_name . '</a>';
 } ?></dt>
 				<dd><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></dd>
 			</dl>
-<?php endwhile;
+<?php
+	endwhile;
 endif;
-the_posts_pagination(array('screen_reader_text'=>'')); ?>
+the_posts_pagination(array('screen_reader_text'=>''));
+?>
 	</div>
 </div>
 <?php get_footer(); ?>
